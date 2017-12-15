c9252538d8be2d0887e5240aae477f518f8058ab
Roicxy Alonso <roicxy@roys-mac-mini.local>
2017-12-15 11:04:04 -0500

    Initial Commit

---
+++ EncryptTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EncryptTest
+//
+//  Created by Roicxy Alonso on 12/15/17.
+//  Copyright © 2017 Roicxy Alonso. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
